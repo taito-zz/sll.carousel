8cd00092d6967f84ed0a3804fc4eec0fe6b0276d
Taito Horiuchi <taito.horiuchi@gmail.com>
2014-06-26 22:53:13 +0300

    update bootstrap.py and fix test

--- src/sll/carousel/upgrades.py
+++ src/sll/carousel/upgrades.py
@@ -1,9 +1,1 @@
-from abita.utils.utils import reimport_profile
-
-
 PROFILE_ID = 'profile-sll.carousel:default'
-
-
-def reimport_viewlets(context, logger=None):
-    """Reimport viewlets"""
-    reimport_profile(context, PROFILE_ID, 'viewlets')
